0ff9c4a779015d9459d9f6d15cd53f15b74d6d14
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-10-30 22:24:03 +0500

    add get method

--- controllers/userControllers.mjs
+++ controllers/userControllers.mjs
@@ -8,3 +8,26 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ message: 'Failed to fetch users' });
     }
 };
+
+export const updateUser = async (req, res) => {
+    const { name, email } = req.body;
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (user) {
+            user.name = name || user.name;
+            user.email = email || user.email;
+
+            const updatedUser = await user.save();
+            res.json({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+            });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to update user' });
+    }
+};
