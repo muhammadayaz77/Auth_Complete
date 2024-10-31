a432340f4cb6a91ebb28309a36e200604f474f88
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-10-31 23:16:35 +0500

    add controllers

--- controllers/userControllers.mjs
+++ controllers/userControllers.mjs
@@ -8,7 +8,19 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ message: 'Failed to fetch users' });
     }
 };
-
+// GET single user by ID
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).select('-password');
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to fetch user' });
+    }
+};
 export const updateUser = async (req, res) => {
     const { name, email } = req.body;
     try {
@@ -31,3 +43,5 @@ export const updateUser = async (req, res) => {
         res.status(500).json({ message: 'Failed to update user' });
     }
 };
+
+
